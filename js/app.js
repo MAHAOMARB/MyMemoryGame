7b8745c8725d1c3183b5c8697ccb40d54d83ad15
MAHAOMARB <38767085+MAHAOMARB@users.noreply.github.com>
2018-05-01 18:06:40 +0300

    remove spaces and some comments ,change some comment

--- js/app.js
+++ js/app.js
@@ -12,42 +12,38 @@ timer.innerHTML=" 0 mins : 0 secs ";//initial time
 var second=0;
 var minute=0;
 
-//...........................................................
-
 displayCards();
-//.........................................................
 
 //Add event listener
 //loop to add event listener to each card
 var cards =document.getElementsByClassName("card");  
-   for(var i=0; i<cards.length; i++){
-      cards[i].addEventListener("click" ,function(){fireMatcher(this)});         
-   } 
+for(var i=0; i<cards.length; i++){
+cards[i].addEventListener("click" ,function(){fireMatcher(this)});         
+} 
 
-//..........................................................
-//function restartBtn to refresh page of game     
+//refresh page of game     
 function restartBtn() {
     window.location.reload();
 }  
-//..........................................................
-//bring all cards from DOM
+
+//access to all initial crads through DOM
 function cardsInitialize() {
 var domCards = document.getElementsByClassName("card"); 
 return transformer(domCards);
 }
 
-//..........................................................
 function transformer(object){
 var transformed=[];
+
 for(var key in object){
     if(object.hasOwnProperty(key)){
         transformed.push(object[key].innerHTML);
     }  
 }
+
 return transformed;
 }
 
-//...........................................................
 //remove all initial cards then display shuffled Cards each time the player wants to play the game
 function displayCards(){
     
@@ -56,6 +52,7 @@ function displayCards(){
     for(var i=0; i< ul.length ; i++){
         ul[i].innerHTML='';
     }
+
     for (var y=0 ; y<shuffledCards.length; y++){
         let elmi = document.createElement("i");
         var li = document.createElement("li");
@@ -65,16 +62,18 @@ function displayCards(){
         ul[0].appendChild(li);
     }      
 }
- //..........................................................
+
 function fireMatcher(card) {
     if(card.className !== 'card match' && card.className !== 'card show open'){
     click++;
-    if(click==1){startTimer();}
+    if(click==1){
+        startTimer();
+    }
     displayIcon(card); 
     isMatch(card);
     }
 }
-//...........................................................
+
 function startTimer(){
     interval=setInterval(function(){
         timer.innerHTML= minute + " mins " + " : " + second + " secs ";
@@ -84,36 +83,30 @@ function startTimer(){
             second=0;
         }
     },1000);   
- } 
- //...........................................................
- //gives score 3,2 or 1 depends on movesCounter
+} 
+
+//gives score 3,2 or 1 depends on movesCounter
 function starRating(movesCounter){
     let score = 3;
-    //var stars=document.getElementsByClassName("stars");
     if(movesCounter <= 18){
         score = 3;
     }   
     else if((movesCounter > 18) && (movesCounter <= 25)){
-        //stars[0].classList.remove("fa fa-star");
         score = 2;
     }    
 	else{
-       // stars[0].classList.remove("fa fa-star");
-       // stars[1].classList.remove("fa fa-star");
         score = 1;
     }
+
     return score;
 }
 
- //...........................................................
- //to show "open" and "show" classes
- function displayIcon(card){
-    
-    card.className +=" "+"show open";
-    
+//to show "open" and "show" classes
+function displayIcon(card){
+    card.className +=" "+"show open";   
 }
- //...................................................................
- //comapre between two cards if they matched or not
+
+//comapre between two cards if they matched or not
 function isMatch(card){
     
     if(openedCard.length==0){
@@ -132,12 +125,12 @@ function isMatch(card){
         } 
     }    
 }
-//......................................................................
+
 function incrementMoves(){
     movesCounter++;
     document.getElementsByClassName("moves")[0].innerHTML=movesCounter;
 }
-//.....................................................................
+
 function starCounter(){
     if (movesCounter === 20) {
         removeStar();
@@ -146,13 +139,13 @@ function starCounter(){
         removeStar();
     }
 }
-//.....................................................................
+
 function removeStar(){
     var star = document.getElementsByClassName("stars")[0];
     var element=star.getElementsByTagName("li");
-        star.removeChild(element[0]);  
+    star.removeChild(element[0]);  
 }
-//.....................................................................
+
 function matched(openedCard){
     setTimeout(function(){
     for(var x=0; x<openedCard.length;x++){
@@ -165,7 +158,7 @@ function matched(openedCard){
     
     }, 1000);  
 }  
-//.....................................................................
+
 function noMatched(openedCard){
     setTimeout(function(){
         for(var z=0; z<openedCard.length;z++){
@@ -174,18 +167,17 @@ function noMatched(openedCard){
         
     }, 1000);   
 } 
-//.....................................................................
+
 //all cards have match class
 function isAllMatched(){
     
     if(allMatchedCards==16){
         clearInterval(interval);
         var grade= starRating(movesCounter);
-        showInfo(grade);
-        
+        showInfo(grade);   
     }
 }
-//.......................................................................
+
 function showInfo(grade){
   
     swal({ 
@@ -203,19 +195,9 @@ function showInfo(grade){
         // handle dismissals
         // result.dismiss can be 'cancel', 'overlay', 'esc' or 'timer'
         }
-        });
+    });
     
 }
-
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -231,14 +213,3 @@ function shuffle(array) {
 
     return array;
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
